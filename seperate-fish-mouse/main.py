ab0e1efdd4d46e150f716d1603ca22a31f66c5a9
yu xiang <yux.uth@gmail.com>
2019-05-23 11:59:55 -0400

    Update main.py
    update parser and use zip for iterator

--- seperate-fish-mouse/main.py
+++ seperate-fish-mouse/main.py
@@ -1,32 +1,26 @@
 import HTSeq
 import gzip
 import subprocess
+from argparse import ArgumentParser
 
 """
-This script is used to seperate fish reads from a fastq file containing reads of
-both fish and mouse.
-(1) Creating a .bam file by mapping the original fastq file to the fish reference genome.
-(2) Finding all matched reads in the bam file using HTseq is_matched methods.
-(3) Using the dictionary produced by (2), iterate through R1 and R2 fastq files to
-    seperate fish reads from mouse.
+seperate fq based on align statues
 """
 
-BAM_FILE = 'result.bam'
-FASTQ_R1 = 'RNA_10_USPD16095262-AK399-AK12890_HY2VKCCXY_L8_1.fq.gz'
-FASTQ_R2 = 'RNA_10_USPD16095262-AK399-AK12890_HY2VKCCXY_L8_2.fq.gz'
+# BAM_FILE = 'result.bam'
+# FASTQ_R1 = 'RNA_10_USPD16095262-AK399-AK12890_HY2VKCCXY_L8_1.fq.gz'
+# FASTQ_R2 = 'RNA_10_USPD16095262-AK399-AK12890_HY2VKCCXY_L8_2.fq.gz'
 # FASTQ_R2 = 'test.fq'
 
 
 def find_matched_reads_in_bam(filename):
     """
-    This method takes the name of a bam file as input, returning a list of names
-    of the reads that is matched to the fish reference genome.
+    return mapped reads name as set
     """
     matched = [] # Names of all matched reads.
 
     all_reads = 0
     aligned_reads = 0
-
     bam_reader = HTSeq.BAM_Reader(filename)
     for align in bam_reader:
         all_reads+=1
@@ -42,39 +36,55 @@ def find_matched_reads_in_bam(filename):
     print("Unique aligned:",len(set(matched)))
     return set(matched)
 
-def catagorize_fastq(matched,filename):
+def catagorize_fastq(matched,r1,r2): ## process two files
     """
-    Catagoriz each read of a fastq file based on whether it is aligned to
-    the reference genome (therefore name appears in matched) or not.
-    Output two files - "filename_aligned" & "filename_unaligned".
+    spit reads by name based on mapped reads name set
     """
-    fastq_reader = HTSeq.FastqReader(filename)
+    fastq_reader1 = HTSeq.FastqReader(r1)
+    fastq_reader2 = HTSeq.FastqReader(r2)
+    filename1 = r1.split('.')[0]
+    filename2 = r2.split('.')[0]
     counter = 0
-
-    aligned_output = open(filename[:-6]+"_aligned.fq", "w" )
-    unaligned_output = open(filename[:-6]+"_unaligned.fq", "w" )
-
-    for read in fastq_reader:
-        if read.name.split(" ")[0] in matched:
-            counter+=1
-            read.write_to_fasta_file(aligned_output)
+    aligned_output1 = open(filename1+"_aligned.fq", "w" )
+    unaligned_output1 = open(filename1+"_unaligned.fq", "w" )
+    
+    aligned_output2 = open(filename2+"_aligned.fq", "w" )
+    unaligned_output2 = open(filename2+"_unaligned.fq", "w" )
+    
+    for read1,read2 in zip(fastq_reader1,fastq_reader2 ):
+        if read1.name.split(" ")[0] in matched:
+            counter += 1
+            read1.write_to_fasta_file(aligned_output1)
+            read2.write_to_fasta_file(aligned_output2)
         else:
-            read.write_to_fasta_file(unaligned_output)
+            read1.write_to_fasta_file(unaligned_output1)
+            read2.write_to_fasta_file(unaligned_output2)
 
-    aligned_output.close()
-    unaligned_output.close()
+    aligned_output1.close()
+    unaligned_output1.close()
+    aligned_output2.close()
+    unaligned_output2.close()
 
     # Compress as .gz
-    subprocess.call(["gzip", filename[:-6]+"_aligned.fq"])
-    subprocess.call(["gzip", filename[:-6]+"_unaligned.fq"])
+    subprocess.call(["gzip", filename1+"_aligned.fq"])
+    subprocess.call(["gzip", filename1+"_unaligned.fq"])
+    subprocess.call(["gzip", filename2+"_aligned.fq"])
+    subprocess.call(["gzip", filename2+"_unaligned.fq"])
 
     print("Find %d aligns in fastq" % counter)
     return 0;
 
 def main():
+    parser = ArgumentParser(description='provide fastq and bam files') ## add parser
+    parser.add_argument('-r1', help='r1', required=True)
+    parser.add_argument('-r2',  help='r2', required=True)
+    parser.add_argument('-bam',  help='aligned bam file', required=True)
+    options = parser.parse_args()
+    FASTQ_R1 = options.r1
+    FASTQ_R2 = options.r2
+    BAM_FILE = options.bam
     matched = find_matched_reads_in_bam(BAM_FILE)
-    catagorize_fastq(matched,FASTQ_R1)
-    catagorize_fastq(matched,FASTQ_R2)
+    catagorize_fastq(matched,FASTQ_R1, FASTQ_R2)
 
 if __name__== "__main__":
     main()
